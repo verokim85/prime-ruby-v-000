20cfb5f4f0619e099ab272869a3526389b4b88d5
Veronica Kim <vero.a.kim@gmail.com>
2018-01-28 19:14:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,22 @@
 def prime?(num)
 
-  if num == 1 || num == -1 || num == 0
-    false
-  elsif num == 2 || num == -2 || num == 3 || num == -3
-    true
-  elsif num % 2 == 0 || num % 3 == 0 || num % 41 == 0
-    false
-  elsif num % 2 == 1 || num % 2 == -1
-    true
+  # if num == 1 || num == -1 || num == 0
+  #   false
+  # elsif num == 2 || num == -2 || num == 3 || num == -3
+  #   true
+  # elsif num % 2 == 0 || num % 3 == 0 || num % 41 == 0
+  #   false
+  # elsif num % 2 == 1 || num % 2 == -1
+  #   true
+  # end
 
-  end
+
+
+
+
+
+
+  
 end
 
 
